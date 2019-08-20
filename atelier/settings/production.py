3d2d7788af19aed249fafd044fd49bbf6c0eb9bf
gicu-90 <45259065+gicu-90@users.noreply.github.com>
2019-08-20 23:25:23 +0300

    Update production.py

--- atelier/settings/production.py
+++ atelier/settings/production.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = 'atelierfinal.herokuapp.com'
+ALLOWED_HOSTS = ['atelierfinal.herokuapp.com','34.241.172.109']
 
 
 # Application definition
